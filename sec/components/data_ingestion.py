62279433a4624d5811a1e94d8a1e6fdcbcb9bc73
sashwot <sashwotkoirala3@gmail.com>
2025-03-30 22:56:20 -0500

    data_ingestion and data_transformation

--- sec/components/data_ingestion.py
+++ sec/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from sec.components.data_transformation import DataTransformation
+from sec.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -50,12 +52,13 @@ class DataIngestion:
             )
 
         except Exception as e:
-            pass 
+            raise CustomException(e, sys)
 
 if __name__ == '__main__':
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
-    
+    train_data, test_data = obj.initiate_data_ingestion()
+    data_transformation = DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
 
 
     
--- sec/components/data_transformation.py
+++ sec/components/data_transformation.py
@@ -12,7 +12,10 @@ from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from exception import CustomException
 from logger import logging
+
 import os
+from sec.utils import save_object
+
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path = os.path.join('artifacts', "preprocessor.pkl")
@@ -21,8 +24,68 @@ class DataTransformation:
     def __init__(self):
         self.data_transformation_config = DataTransformationConfig()
     def get_data_transformation_object(self):
+        '''
+        This function is responsible for transformation.
+        '''
         try:
-            pass
-        except:
-            pass
+            numerical_columns =['Close','High','Low','Open','Volume']
+            num_pipeline = Pipeline(steps = [ "imputer", SimpleImputer(strategy='median'),
+                                    "Scaler",StandardScaler() 
+                                    ])
+            logging.info("Numerical col standard scaling completed.")
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline", num_pipeline, numerical_columns )
+                    
+                ]
+            )
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e,sys)
+    
+    def initiate_data_transformation(self, train_path, test_path):
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info('Read train and test data completed')
+            logging.info('Obtaining preprocessing object')
+            preprocessing_obj = self.get_data_transformation_object()
+            target_column_name='Open'
+            numerical_columns =['Close','High','Low','Volume']
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
+
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
+            )
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_[
+                input_feature_train_arr, np.array(target_feature_train_df)
+            ]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object.")
+
+            save_object(
+
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+                )
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,)   
+
+            
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+
+
         
